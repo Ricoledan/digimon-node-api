049713268efd30c7c914c36505c9635c0c81bb6c
Ricardo Ledan <ricoledan@gmail.com>
2021-04-11 01:11:38 -0400

    add notes

--- src/routes/profile.route.ts
+++ src/routes/profile.route.ts
@@ -9,4 +9,12 @@ router.post('/profile', ProfileController.createProfile)
 router.patch('/profile/:name', ProfileController.updateProfile)
 router.delete('/profile/:name', ProfileController.deleteProfile)
 
+//* TODO: work to support frontend application
+// problem: upload image to S3 bucket and store url in db
+// implementation: create endpoint that does that work
+// code: router.get('/upload/:image', ProfileController.uploadProfileImage)
+
+// implementation note: upload can happen as part of create in the service, service calls the upload endpoint if image is found,
+// also lives as sepaerate endpoint if needed
+
 export default router
