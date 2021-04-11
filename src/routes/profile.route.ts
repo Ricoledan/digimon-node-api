e892ee221266ec6654c0a0ec7283de6f3eaad98c
Ricardo Ledan <ricoledan@gmail.com>
2021-04-11 00:16:10 -0400

    update put to patch

--- src/routes/profile.route.ts
+++ src/routes/profile.route.ts
@@ -6,7 +6,7 @@ const router: Router = Router()
 router.get('/profile', ProfileController.getAllProfiles)
 router.get('/profile/:name', ProfileController.getProfileByName)
 router.post('/profile', ProfileController.createProfile)
-router.put('/profile/:name', ProfileController.updateProfile)
+router.patch('/profile/:name', ProfileController.updateProfile)
 router.delete('/profile/:name', ProfileController.deleteProfile)
 
 export default router
