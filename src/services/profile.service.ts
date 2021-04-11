02204babfb38c20b61b4e5421fb4ee8ebd272d9a
Ricardo Ledan <ricoledan@gmail.com>
2021-04-11 00:26:35 -0400

    add repository to service

--- src/services/profile.service.ts
+++ src/services/profile.service.ts
@@ -1,18 +1,19 @@
+import ProfileRepository from '../repositories/profile.repository'
 class ProfileService {
   async getAll(req: any, res: any) {
-    return await res.send('getAll')
+    return await ProfileRepository.readAll(req, res)
   }
   async getByName(req: any, res: any) {
-    return await res.send('getProfileByName')
+    return await ProfileRepository.readOne(req, res)
   }
   async create(req: any, res: any) {
-    return await res.send('create')
+    return await ProfileRepository.create(req, res)
   }
   async update(req: any, res: any) {
-    return await res.send('update')
+    return await ProfileRepository.update(req, res)
   }
   async delete(req: any, res: any) {
-    return await res.send('deleteData')
+    return await ProfileRepository.delete(req, res)
   }
 }
 
