2daf08092367639f85800256c288a3cc1699170a
Ricardo Ledan <ricoledan@gmail.com>
2021-04-12 17:05:55 -0400

    scaffold create profile logic

--- src/repositories/profile.repository.ts
+++ src/repositories/profile.repository.ts
@@ -1,5 +1,8 @@
 import client from '../config/db.config'
 
+// add time date library
+const now = ''
+
 class ProfileRepository {
   async readAll(req: any, res: any) {
     try {
@@ -19,18 +22,46 @@ class ProfileRepository {
     const getName = req.params.name
 
     try {
-      const result = await client
+      const results = await client
         .db('digimon')
         .collection('profile')
         .findOne({ name: getName })
 
-      return res.send(result)
+      return res.send(results)
     } catch (e) {
       console.error(e)
     }
     client.close()
   }
   async create(req: any, res: any) {
+    const getRequestBody = req.body
+    const createQuery = {
+      __v: 0,
+      name: getRequestBody.name,
+      level: getRequestBody.level,
+      type: getRequestBody.type,
+      attribute: getRequestBody.attribute,
+      field: getRequestBody.field ?? null,
+      group: getRequestBody.group ?? null,
+      technique: getRequestBody.technique,
+      artwork: getRequestBody.artwork,
+      profile: getRequestBody.profile,
+      timestamp: {
+        created_at: now,
+        updated_at: null,
+        deleted_at: null
+      }
+    }
+    try {
+      const results = await client
+        .db('digimon')
+        .collection('profile')
+        .insertOne(createQuery)
+
+      return res.send(results)
+    } catch (e: any) {
+      console.error(e)
+    }
     return res.send('create profile')
   }
   async update(req: any, res: any) {
