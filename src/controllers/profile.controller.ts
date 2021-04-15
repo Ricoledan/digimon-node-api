436724d00e4e5c1fd79c51b01de1dfe274249be1
Ricardo Ledan <ricoledan@gmail.com>
2021-04-15 01:25:12 -0400

    add express types to res req

--- src/controllers/profile.controller.ts
+++ src/controllers/profile.controller.ts
@@ -1,20 +1,20 @@
 import ProfileService from '../services/profile.service'
-
+import type { Request, Response } from 'express'
 class ProfileController {
-  async getAllProfiles(req: any, res: any) {
+  async getAllProfiles(req: Request, res: Response) {
     return await ProfileService.getAll(req, res)
   }
-  async getProfileByName(req: any, res: any) {
+  async getProfileByName(req: Request, res: Response) {
     return await ProfileService.getByName(req, res)
   }
 
-  async createProfile(req: any, res: any) {
+  async createProfile(req: Request, res: Response) {
     return await ProfileService.create(req, res)
   }
-  async updateProfile(req: any, res: any) {
+  async updateProfile(req: Request, res: Response) {
     return await ProfileService.update(req, res)
   }
-  async deleteProfile(req: any, res: any) {
+  async deleteProfile(req: Request, res: Response) {
     return await ProfileService.delete(req, res)
   }
 }
