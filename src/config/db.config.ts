30f33525344b24cad1bdc7e5effb7abb65aed367
Ricardo Ledan <ricoledan@gmail.com>
2021-04-17 02:28:18 -0400

    remove db reference

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -14,7 +14,7 @@ class Database {
     const connectionUri =
       process.env.NODE_ENV === 'prod'
         ? `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@yggdrasil.ug5tw.mongodb.net/digimon?retryWrites=true&w=majority`
-        : `mongodb://${process.env.DOCKER_DB_USER}:${process.env.DOCKER_DB_PASSWORD}@127.0.0.1:27017/digimon`
+        : `mongodb://${process.env.DOCKER_DB_USER}:${process.env.DOCKER_DB_PASSWORD}@127.0.0.1:27017`
     this.client = new mongodb.MongoClient(connectionUri, {
       useNewUrlParser: true,
       useUnifiedTopology: true
