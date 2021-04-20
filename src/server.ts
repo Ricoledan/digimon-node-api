7bcdc372d3d0651150f9ec5c2ae7bfa51b96c340
Ricardo Ledan <ricoledan@gmail.com>
2021-04-19 20:40:54 -0400

    add esrouter test and added chalk to project

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,9 @@ import * as dotenv from 'dotenv'
 import express, { Request, Response } from 'express'
 import cors from 'cors'
 import helmet from 'helmet'
+import chalk from 'chalk'
 import ProfileRouter from './routes/profile.route'
+import EsRouter from './routes/es.route'
 
 dotenv.config()
 
@@ -17,11 +19,14 @@ app.use(helmet())
 app.use(cors())
 app.use(express.json())
 
-app.use('/api', ProfileRouter)
+app.use('/api', ProfileRouter, EsRouter)
 app.get('/', (req: Request, res: Response) => {
   res.send('Digimon API')
 })
 
 app.listen(PORT, () => {
-  console.log(`[デジタルモンスター]: running on http://localhost:${PORT}`)
+  console.log(
+    chalk.bgCyan(`[デジタルモンスター]`),
+    chalk.bold.blue(`: running on http://localhost:${PORT}`)
+  )
 })
