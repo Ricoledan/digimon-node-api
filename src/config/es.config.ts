43ca95bed974dab0e2580fe22a21a88b8de0079c
Ricardo Ledan <ricoledan@gmail.com>
2021-04-19 20:42:34 -0400

    add es config

---
+++ src/config/es.config.ts
@@ -0,0 +1,24 @@
+import dotenv from 'dotenv'
+import elasticSearch from 'elasticsearch'
+
+dotenv.config()
+
+class ElasticSearch {
+  client: any
+
+  constructor() {
+    this.connect()
+  }
+
+  async connect() {
+    this.client = new elasticSearch.Client({
+      host: 'localhost:9200'
+    })
+    await this.client
+      .connect()
+      .then(console.log('connected to elasticsearch'))
+      .catch((e: string) => console.error(`Fatal error occurred: ${e}`))
+  }
+}
+
+export default new ElasticSearch().client
