fc42adb78eb9496e6c58e0f968e7f72ef8c612f1
Ricardo Ledan <ricoledan@gmail.com>
2021-04-21 19:29:30 -0400

    implement mongo

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,5 +1,5 @@
 import dotenv from 'dotenv'
-import mongoose from 'mongoose'
+import mongodb from 'mongodb'
 
 dotenv.config()
 
@@ -11,31 +11,21 @@ class Database {
   }
 
   async connect() {
-    const uri =
+    const connectionUri =
       process.env.NODE_ENV === 'prod'
         ? `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@yggdrasil.ug5tw.mongodb.net/digimon?retryWrites=true&w=majority`
-        : `mongodb://${process.env.DOCKER_DB_USER}:${process.env.DOCKER_DB_PASSWORD}@127.0.0.1:27017/`
-
-    const options = {
+        : `mongodb://${process.env.DOCKER_DB_USER}:${process.env.DOCKER_DB_PASSWORD}@127.0.0.1:27017`
+    this.client = new mongodb.MongoClient(connectionUri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       connectTimeoutMS: 30000,
       keepAlive: true
-    }
-
-    this.client = mongoose.connect(uri, options)
-
-    await this.client.connect()
-    .then(console.log('connected to mongodb'))
-    .catch((e: string) => console.error(`Fatal error occurred: ${e}`))
-
-  //   this.client = new mongodb.MongoClient(uri, options)
-
-  //   await this.client
-  //     .connect()
-  //     .then(console.log('connected to mongodb'))
-  //     .catch((e: string) => console.error(`Fatal error occurred: ${e}`))
-  // }
+    })
+    await this.client
+      .connect()
+      .then(console.log('connected to mongodb'))
+      .catch((e: string) => console.error(`Fatal error occurred: ${e}`))
+  }
 }
 
 export default new Database().client
