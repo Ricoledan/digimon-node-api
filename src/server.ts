a1bf79eb2dc2dcdf64f50e468e65c5e29eae6c82
Ricardo Ledan <ricoledan@gmail.com>
2021-04-21 19:31:31 -0400

    remove upload route

--- src/server.ts
+++ src/server.ts
@@ -18,6 +18,7 @@ const PORT: number = parseInt(process.env.PORT as string, 10)
 app.use(helmet())
 app.use(cors())
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.use('/api', ProfileRouter, EsRouter)
 app.get('/', (req: Request, res: Response) => {
