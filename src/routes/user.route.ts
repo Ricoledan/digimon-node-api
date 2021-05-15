3a1caddaa370b4ec3fb96ecca09ec7a725a3c93f
Ricardo Ledan <ricoledan@gmail.com>
2021-05-15 04:33:27 -0400

    add email param to routes

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -4,9 +4,9 @@ import UserController from '../controllers/user.controller'
 const router: Router = Router()
 
 router.get('/users', [], UserController.getUsers)
-router.get('/users/:name', [], UserController.getUsers)
+router.get('/users/:email', [], UserController.getUsers)
 router.post('/users', [], UserController.createUsers)
-router.patch('/users/:name', [], UserController.updateUsers)
-router.delete('/users/:name', [], UserController.deleteUsers)
+router.patch('/users/:email', [], UserController.updateUsers)
+router.delete('/users/:email', [], UserController.deleteUsers)
 
 export default router
