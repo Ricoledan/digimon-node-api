ceb579f82662b933d135a643c5b9002b789e5963
Ricardo Ledan <ricoledan@gmail.com>
2021-05-17 01:20:43 -0400

    updates

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,10 @@ import express, { Request, Response } from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
 import helmet from 'helmet'
-import chalk from 'chalk'
 import logger from './lib/logger'
 import morgan from './config/morgan'
+import compression from 'compression'
+import chalk from 'chalk'
 import ProfileRouter from './routes/profile.route'
 import UserRouter from './routes/user.route'
 import EsRouter from './routes/es.route'
@@ -18,7 +19,6 @@ if (!process.env.PORT) {
 }
 
 const app = express()
-const PORT: number = parseInt(process.env.PORT as string, 10)
 
 const connectionUri =
   process.env.NODE_ENV === 'prod'
@@ -43,15 +43,18 @@ app.use(helmet())
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(compression())
 app.use(morgan)
 
 app.use('/api', ProfileRouter, UserRouter, EsRouter, LogsRouter)
 app.get('/', (req: Request, res: Response) => {
-  res.send('Digimon API')
+  res.send('digimon api')
 })
 
+const PORT: number = parseInt(process.env.PORT as string, 10)
+
 app.listen(PORT, () => {
-  console.log(
+  logger.info(
     chalk.bgCyan(`[デジタルモンスター]`),
     chalk.bold.blue(`: running on http://localhost:${PORT}`)
   )
