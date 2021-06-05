584b76783482efb4d15119276588a0a624165087
Ricardo Ledan <ricoledan@gmail.com>
2021-06-05 02:52:20 -0400

    remove empty test

--- src/__tests__/date.time.ts
+++
@@ -1,6 +0,0 @@
-// import { now } from '../helpers/date.time'
-// add mock for time
-// describe('now()', () => {
-//   it('should return current datetime', () => {})
-//   expect(now().toString()).toBe(new Date('2019-01-01'))
-// })
