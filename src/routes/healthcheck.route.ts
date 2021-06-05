9514323435bf6e8e729129ecc71090893bd48eaa
Ricardo Ledan <ricoledan@gmail.com>
2021-06-05 09:40:30 -0400

    add note

--- src/routes/healthcheck.route.ts
+++ src/routes/healthcheck.route.ts
@@ -2,6 +2,8 @@ import { Router, Request, Response } from 'express'
 
 const router: Router = Router()
 
+// note: build healthcheck that checks for db connections and other integral parts of the application.
+
 router.get('/health-check', (req: Request, res: Response) => {
   res.send('healthcheck')
 })
