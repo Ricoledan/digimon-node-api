839334a3813c2f9a0b6946fe81c4da02f4b82eea
Ricardo <ricoledan@gmail.com>
2021-08-15 10:46:53 -0400

    misc edits

--- src/__tests__/api.test.ts
+++ src/__tests__/api.test.ts
@@ -1,13 +1,14 @@
 import request from 'supertest'
 import server from '../server'
 
-describe('GET root response', () => {
+describe('GET requests return with 200', () => {
   it('GET root response', (done) => {
     request(server).get('/').expect(200, '[デジタルモンスター]', done)
   })
   // it('GET Profiles', (done) => {
   //   request(server)
   //     .get('/api/profiles')
+  //     .set('Content-Type', 'application/json')
   //     .expect('Content-Type', /json/)
   //     .expect(200, done)
   // })
